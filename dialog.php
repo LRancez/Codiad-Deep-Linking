e86fffba241f395363b8910d8dff38cf9c872a8d
Lucas Rancez <lucas.rancez@fox.com>
2013-10-11 12:08:09 -0300

    fully select the link inside the popup

--- dialog.php
+++ dialog.php
@@ -1,4 +1,7 @@
 <form>
-<input type="text" readonly value="<?php echo($_GET['path']); ?>">
+<input type="text" readonly value="<?php echo($_GET['path']); ?>" onmouseup="this.select();">
 <button class="btn-right" onclick="window.open($(this).parent().children('input').val(), '_blank'); return false;">Open in new Codiad</button>
+<script type="text/javascript">
+	$('#modal-content input').focus();
+</script>
 </form>
